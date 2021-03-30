67594a550ff0341b15ba60992c0425900514c75a
Milos Vuksanovic <hex3solutions@gmail.com>
2021-03-30 19:53:59 +0200

    Finishing task #5

--- JS-Sql/src/index.js
+++ JS-Sql/src/index.js
@@ -2,13 +2,27 @@ import express from 'express';
 import { env } from './env';
 import router from './routes';
 import db from '../database/models';
+import logging from './helpers/logger';
 
-db.sequelize.sync();
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-app.listen(env.app.port, () => {
-	console.log(`App running on ${env.app.port}`);
+db.sequelize.sync().then(() => {
+	try {
+		app.listen(env.app.port, () => {
+			logging.info(
+				'SERVER',
+				`APP IS RUNNING ${env.app.port}`,
+			);
+		});
+		logging.info('SERVER', 'CONNECTED TO POSTGRES PORT 5432');
+	} catch (error) {
+		logging.error(
+			'SERVER',
+			'CONNECT TO POSTGRES FAILED',
+			error.message,
+		);
+	}
 });
