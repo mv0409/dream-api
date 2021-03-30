cf4ea39b2ffde16c4ef543b3476da4333505735b
Milos Vuksanovic <hex3solutions@gmail.com>
2021-03-30 15:08:35 +0200

    refactor

--- src/helpers/interfaces/controller.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Request } from 'express';
-import { HttpResponse } from './http-response';
-
-export interface ControllerInterface {
-	(req: Request): Promise<HttpResponse>;
-}
--- src/helpers/interfaces/dream.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface DreamInterface {
-	title: string;
-	description: string;
-	date: string;
-	type: string;
-}
