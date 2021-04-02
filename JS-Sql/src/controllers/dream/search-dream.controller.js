44151d667819a5994ec19cd599e86f34d8ca0bc7
Milos Vuksanovic <hex3solutions@gmail.com>
2021-04-02 15:36:35 +0200

    Add paginate and transform to controller

--- JS-Sql/src/controllers/dream/search-dream.controller.js
+++ JS-Sql/src/controllers/dream/search-dream.controller.js
@@ -1,99 +1,28 @@
 import HttpError from '../../helpers/errors/http-error';
 import { Dream } from '../../../database/models';
+import { transformDreamSearch } from '../../helpers/transform/search-dream';
+import { paginate } from '../../helpers/pagination';
 
 export const SearchDreamController = () => {
 	return async (req) => {
 		try {
-			const result = req.body
-			const page = parseInt(req.query.page);
-			const limit = parseInt(req.query.limit)
-			const pagination = {};
-			const startIndex = (page - 1) * limit;
-			const endIndex = page * limit;
+			const result = transformDreamSearch(req);
 
-			if(result.dateBetween) {
-				const startDate = new Date(result.dateBetween.startDate.toString())
-				const endDate = new Date(result.dateBetween.endDate.toString())
-				const {count, rows} = await Dream.findAndCountAll({
-					from: {
-						$between: [startDate, endDate]
-					},
-					where: result,
-					offset: startIndex,
-					limit: limit,
-				});
-				if(count == 0) {
-					return HttpError({
-						statusCode: 409,
-						errorMessage: 'No data'
-					})
-				}
+			const { count, rows } = await Dream.findAndCountAll(
+				result,
+			);
 
-				if (startIndex > 0) {
-					pagination.previous = {
-						page: page - 1,
-						limit: limit,
-					};
-				}
-			
-				if (endIndex < count) {
-					pagination.next = {
-						page: page + 1,
-						limit: limit,
-					};
-				}
-
-				return {
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					statusCode: 200,
-					data: {
-						success: true,
-						payload: rows,
-						pagination: pagintion,
-					},
-				};
-			}
-
-			const {count, rows} = await Dream.findAndCountAll({
-				where: result,
-				offset: startIndex,
-				limit: limit,
-			});
-
-			
-
-			if(count == 0) {
-				return HttpError({
-					statusCode: 409,
-					errorMessage: 'No data'
-				})
-			}
-
-			if (startIndex > 0) {
-				pagination.previous = {
-					page: page - 1,
-					limit: limit,
-				};
-			}
-		
-			if (endIndex < count) {
-				pagination.next = {
-					page: page + 1,
-					limit: limit,
-				};
-			}
-
+			const pagination = paginate(req.params, count);
 			return {
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				statusCode: 200,
+				statusCode: 201,
 				data: {
 					success: true,
 					payload: rows,
-					pagination: pagination,
+					count,
+					pagination,
 				},
 			};
 		} catch (error) {
