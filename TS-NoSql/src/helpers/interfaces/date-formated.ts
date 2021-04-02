ed31240ad09deee842e905930464521ea16dadaa
Milos Vuksanovic <hex3solutions@gmail.com>
2021-04-02 18:46:16 +0200

    Configure Prettier And Run

--- TS-NoSql/src/helpers/interfaces/date-formated.ts
+++ TS-NoSql/src/helpers/interfaces/date-formated.ts
@@ -1,18 +1,16 @@
-export class DateFormated{
-    date!: {}
-    constructor(gt: Date, lt: Date ) {
-        if(gt > lt) {
-            this.date = {
-                $gt : new Date(lt),
-                $lt : new Date(gt)
-            }
-
-        } else {
-            this.date = {
-                $gt : new Date(gt),
-                $lt : new Date(lt)
-
-            }
-        }
-    }
+export class DateFormated {
+	date!: {};
+	constructor(gt: Date, lt: Date) {
+		if (gt > lt) {
+			this.date = {
+				$gt: new Date(lt),
+				$lt: new Date(gt),
+			};
+		} else {
+			this.date = {
+				$gt: new Date(gt),
+				$lt: new Date(lt),
+			};
+		}
+	}
 }
