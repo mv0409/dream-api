26b9ad52b4c0abe6467cd35424f0007f4443c779
Milos Vuksanovic <hex3solutions@gmail.com>
2021-04-04 10:36:36 +0200

    Fix format

--- JS-Sql/src/index.js
+++ JS-Sql/src/index.js
@@ -9,16 +9,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
-db.sequelize.sync().then(() => {
+app.listen(env.app.port, async () => {
 	try {
-		app.listen(env.app.port, () => {
-			logging.info(
-				'SERVER',
-				`APP IS RUNNING ${env.app.port}`,
-			);
-		});
-		logging.info('SERVER', 'CONNECTED TO POSTGRES PORT 5432');
+		await db.sequelize.sync();
+		logging.info('SERVER', `APP IS RUNNING ${env.app.port}`);
 	} catch (error) {
+		logging.info('SERVER', 'CONNECTED TO POSTGRES PORT 5432');
 		logging.error(
 			'SERVER',
 			'CONNECT TO POSTGRES FAILED',
