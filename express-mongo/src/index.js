5a22721ece0917a857970a2f3df9860f98581224
Milos Vuksanovic <hex3solutions@gmail.com>
2021-09-16 13:14:28 +0200

    Refactor source index file

--- express-mongo/src/index.js
+++ express-mongo/src/index.js
@@ -1,4 +1,26 @@
-const App = require('./app');
+'use strict';
 
-const app = new App();
-app.init();
+const App = require('./app');
+const mongoose = require('mongoose');
+const config = require('./config.default');
+
+// MongoDB IIFE
+const connectRetry = (() => {
+	mongoose
+		.connect(config.mongoUri, {
+			// MongoDB configuration
+			useNewUrlParser: true,
+			useFindAndModify: false,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			// Start app
+			App.listen(config.port);
+		})
+		.catch((err) => {
+			console.log('💥 Mongoose connection error:', err);
+			// Retry connection
+			setTimeout(connectRetry, 5000);
+		});
+})();
