0dd36f9dfaac50ffbb76299d7da81bac30f7f5b5
Milos Vuksanovic <hex3solutions@gmail.com>
2021-09-16 13:21:27 +0200

    Add error class that extends Error

---
+++ express-mongo/src/common/errors/http-error.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class ErrorHandler extends Error {
+	constructor(code, message) {
+		super();
+		this.code = code;
+		this.message = message;
+	}
+}
+
+module.exports = ErrorHandler;
