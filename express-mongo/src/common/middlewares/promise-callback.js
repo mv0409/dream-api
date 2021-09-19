91d6df5ad7446d6269ccde3f1e759e49d52338c9
Milos Vuksanovic <hex3solutions@gmail.com>
2021-09-19 23:07:07 +0200

    Set logic ffor to ignore console log in e2e tests

--- express-mongo/src/common/middlewares/promise-callback.js
+++ express-mongo/src/common/middlewares/promise-callback.js
@@ -1,28 +1,35 @@
 'use strict';
 
+const config = require('../../config.default');
 const handleError = require('../utils/handle-error');
 const handleResponse = require('../utils/handle-response');
 const logger = require('../utils/logger');
 
 const callback = (controller) => {
 	return (req, res) => {
-		logger.info(
-			'SERVER',
-			`METHOD: [${req.method}] - ` +
-				`URL: [${req.url}] - ` +
-				`STATUS: [${req.statusCode}]- ` +
-				`IP: [${req.socket.remoteAddress}] `,
-		);
+		// skip console log on e2e test
+		if(config.environment === 'dev') {
+			logger.info(
+				'SERVER',
+				`METHOD: [${req.method}] - ` +
+					`URL: [${req.url}] - ` +
+					`STATUS: [${req.statusCode}]- ` +
+					`IP: [${req.socket.remoteAddress}] `,
+			);
+		}
 		controller(req, res)
 			.then((data) => {
 				handleResponse(res, data);
-				logger.info(
-					'SERVER',
-					`METHOD: [${req.method}] - ` +
-						`URL: [${req.url}] - ` +
-						`STATUS: [${res.statusCode}] - ` +
-						`IP: [${req.socket.remoteAddress}] `,
-				);
+				// skip console log on e2e test
+				if(config.environment === 'dev') {
+					logger.info(
+						'SERVER',
+						`METHOD: [${req.method}] - ` +
+							`URL: [${req.url}] - ` +
+							`STATUS: [${res.statusCode}] - ` +
+							`IP: [${req.socket.remoteAddress}] `,
+					);
+				}
 			})
 			.catch((err) => {
 				handleError(res, err);
