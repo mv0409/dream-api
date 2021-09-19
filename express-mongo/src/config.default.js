51245058bfcfb0f164a54a5d598e73f99a4789e0
Milos Vuksanovic <hex3solutions@gmail.com>
2021-09-19 23:03:53 +0200

    Add environment vars to project

--- express-mongo/src/config.default.js
+++ express-mongo/src/config.default.js
@@ -8,9 +8,11 @@ module.exports = {
 	// Server port
 	port: Number(process.env.PORT) || 3000,
 	// Configuration for MongoDB
-	mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/js-mongo',
+	mongoUri: process.env.ENVIRONMENT === 'test'? process.env.MONGO_URI_TEST : process.env.MONGO_URI,
 	// Public domain of Dream
 	publicDomain: process.env.PUBLIC_DOMAIN || 'localhost:3000',
 	// App name
 	appNAme: process.env.APP_NAME || 'dream',
+	// environment
+	environment:  process.env.ENVIRONMENT === 'test'? 'test' : 'dev'
 };
