0cda63a931e7f4e5bb055ba343d41206acce1de5
Milos Vuksanovic <hex3solutions@gmail.com>
2021-10-01 13:49:08 +0200

    IImplement findandcount typeorm method

--- express-ts-sql/src/entities/dream/DreamService.ts
+++ express-ts-sql/src/entities/dream/DreamService.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
-import { getRepository, Repository, Connection } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 import ErrorHandler from '../../common/errors/HttpError';
 import { Dream } from './DreamEntity';
-import { updateDreamDto } from './dto/updateDream.dto';
+
 
 class DreamService {
 	private readonly _dreamRepo: Repository<Dream>;
@@ -11,19 +11,16 @@ class DreamService {
 	}
 
 	async findAll(req: Request, res: Response) {
-		console.log(req.body);
-		console.log(res.writable);
-		const dreams = await this._dreamRepo.find();
-		return dreams;
-		// 	.find(res.locals.queryObj)
-		// 	.limit(req.query.limit)
-		// 	.skip((req.query.page - 1) * req.query.limit);
-		// const count = await this.dreamModel.countDocuments(res.locals.queryObj);
-		// const parrallelPromise = await Promise.all([dreams, count]);
-		// return {
-		// 	data: parrallelPromise[0],
-		// 	count: parrallelPromise[1],
-		// };
+		const [results,total  ] = await this._dreamRepo
+			.findAndCount({
+				where: res.locals.queryObj,
+				take: Number(req.query.limit),
+				skip: (Number(req.query.page) - 1) * Number(req.query.limit)
+			})
+		return {
+			data: results,
+			count: total
+		}
 	}
 
 	async create(createDreamDto: any) {
