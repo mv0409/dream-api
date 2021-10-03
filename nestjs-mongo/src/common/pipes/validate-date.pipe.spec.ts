691bbad43b0a5e8f744b2466469ae077125004e1
Milos Vuksanovic <hex3solutions@gmail.com>
2021-10-03 20:40:49 +0200

    Add date pipe test

---
+++ nestjs-mongo/src/common/pipes/validate-date.pipe.spec.ts
@@ -0,0 +1,28 @@
+import { ArgumentMetadata } from '@nestjs/common';
+import { ObjectId } from 'mongodb';
+import { ValidateDatePipe } from './validate-date.pipe'
+
+
+describe('date validation pipe', () => {
+  let validateDatePipe: ValidateDatePipe;
+  
+  beforeEach(() => {
+    validateDatePipe = new ValidateDatePipe();
+  });
+
+  it('pipe should not throw err if valid date format passed', () => {
+    const date = '2020-11-11';
+    expect(() => validateDatePipe.transform(date)).not.toThrow();
+  });
+
+  it('pipe should cast date value to if valid', () => {
+    const date = '2020-11-11';
+    const res = validateDatePipe.transform(date);
+    expect(res).toEqual(new Date(date));
+  });
+
+  it('pipe should throw err if invalid date instace', () => {
+    const date = 'invalidDate';
+    expect(() => validateDatePipe.transform(date)).toThrow();
+  });
+});
